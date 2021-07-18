11aeafc84996d070c62beb9f82961761e60e1b0e
Marley <manfrededun@gmail.com>
2021-07-18 09:30:18 +0000

    error fix #2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -56,6 +56,10 @@ class login extends React.Component {
 }
 
 const mapDispatchToProps = { Login };
-
+const mapStateToProps = (state) =>{
+  return {
+  auth : state.firebaseState.auth
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(login);
